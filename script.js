191bf393bd6a7dcc89ce857c1ba6b85a568ec482
yuga-exe <185314560+yuga-exe@users.noreply.github.com>
2026-03-11 22:31:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,11 @@ attribution:'© OpenStreetMap'
 
 let shelters = [
 
-/* KURNOOL */
-
 {lat:15.8333,lng:78.0500,name:"Kurnool Shelter 1"},
 {lat:15.8360,lng:78.0520,name:"Kurnool Shelter 2"},
 {lat:15.8310,lng:78.0470,name:"Kurnool Shelter 3"},
 {lat:15.8290,lng:78.0540,name:"Kurnool Shelter 4"},
 {lat:15.8350,lng:78.0580,name:"Kurnool Shelter 5"},
-
-
-/* NANDYAL */
 
 {lat:15.4833,lng:78.4833,name:"Nandyal Shelter 1"},
 {lat:15.4860,lng:78.4860,name:"Nandyal Shelter 2"},
@@ -24,17 +19,11 @@ let shelters = [
 {lat:15.4790,lng:78.4880,name:"Nandyal Shelter 4"},
 {lat:15.4850,lng:78.4900,name:"Nandyal Shelter 5"},
 
-
-/* KADAPA */
-
 {lat:14.4667,lng:78.8167,name:"Kadapa Shelter 1"},
 {lat:14.4690,lng:78.8200,name:"Kadapa Shelter 2"},
 {lat:14.4630,lng:78.8140,name:"Kadapa Shelter 3"},
 {lat:14.4700,lng:78.8220,name:"Kadapa Shelter 4"},
 {lat:14.4680,lng:78.8100,name:"Kadapa Shelter 5"},
-
-
-/* TIRUPATI */
 
 {lat:13.6288,lng:79.4192,name:"Tirupati Shelter 1"},
 {lat:13.6310,lng:79.4220,name:"Tirupati Shelter 2"},
@@ -50,7 +39,7 @@ let nearestShelter=null;
 
 function showShelters(){
 
-let markers=[];
+let bounds=[];
 
 shelters.forEach(function(s){
 
@@ -58,11 +47,11 @@ L.marker([s.lat,s.lng])
 .addTo(map)
 .bindPopup(s.name);
 
-markers.push([s.lat,s.lng]);
+bounds.push([s.lat,s.lng]);
 
 });
 
-map.fitBounds(markers);
+map.fitBounds(bounds);
 
 }
 
