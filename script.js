4cfe60b95642f0538dec1555c02162df8d530ad8
yuga-exe <185314560+yuga-exe@users.noreply.github.com>
2026-03-11 22:16:18 +0530

    Refactor shelter comments and improve map setup

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let map = L.map('map').setView([15.8,78.0],7);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+attribution:'© OpenStreetMap'
+}).addTo(map);
+
+
 let shelters = [
 
-/* KURNOOL (15.8333 , 78.0500) */
+/* KURNOOL */
 
 {lat:15.8333,lng:78.0500,name:"Kurnool Shelter 1"},
 {lat:15.8360,lng:78.0520,name:"Kurnool Shelter 2"},
@@ -9,7 +16,7 @@ let shelters = [
 {lat:15.8350,lng:78.0580,name:"Kurnool Shelter 5"},
 
 
-/* NANDYAL (15.4833 , 78.4833) */
+/* NANDYAL */
 
 {lat:15.4833,lng:78.4833,name:"Nandyal Shelter 1"},
 {lat:15.4860,lng:78.4860,name:"Nandyal Shelter 2"},
@@ -18,7 +25,7 @@ let shelters = [
 {lat:15.4850,lng:78.4900,name:"Nandyal Shelter 5"},
 
 
-/* KADAPA (14.4667 , 78.8167) */
+/* KADAPA */
 
 {lat:14.4667,lng:78.8167,name:"Kadapa Shelter 1"},
 {lat:14.4690,lng:78.8200,name:"Kadapa Shelter 2"},
@@ -27,7 +34,7 @@ let shelters = [
 {lat:14.4680,lng:78.8100,name:"Kadapa Shelter 5"},
 
 
-/* TIRUPATI (13.6288 , 79.4192) */
+/* TIRUPATI */
 
 {lat:13.6288,lng:79.4192,name:"Tirupati Shelter 1"},
 {lat:13.6310,lng:79.4220,name:"Tirupati Shelter 2"},
@@ -36,3 +43,106 @@ let shelters = [
 {lat:13.6290,lng:79.4150,name:"Tirupati Shelter 5"}
 
 ];
+
+let userLocation=null;
+let nearestShelter=null;
+
+
+function showShelters(){
+
+shelters.forEach(function(s){
+
+L.marker([s.lat,s.lng])
+.addTo(map)
+.bindPopup(s.name);
+
+});
+
+}
+
+
+function getLocation(){
+
+navigator.geolocation.getCurrentPosition(function(pos){
+
+let lat=pos.coords.latitude;
+let lng=pos.coords.longitude;
+
+userLocation=[lat,lng];
+
+L.marker(userLocation)
+.addTo(map)
+.bindPopup("Your Location")
+.openPopup();
+
+map.setView(userLocation,10);
+
+});
+
+}
+
+
+function distance(a,b,c,d){
+
+return Math.sqrt((a-c)**2+(b-d)**2);
+
+}
+
+
+function findNearest(){
+
+if(!userLocation){
+
+alert("Click My Location first");
+return;
+
+}
+
+let min=999;
+
+shelters.forEach(function(s){
+
+let d=distance(userLocation[0],userLocation[1],s.lat,s.lng);
+
+if(d<min){
+
+min=d;
+nearestShelter=s;
+
+}
+
+});
+
+L.marker([nearestShelter.lat,nearestShelter.lng])
+.addTo(map)
+.bindPopup("Nearest Shelter: "+nearestShelter.name)
+.openPopup();
+
+}
+
+
+function showRoute(){
+
+if(!nearestShelter||!userLocation){
+
+alert("Find nearest shelter first");
+return;
+
+}
+
+let lat=userLocation[0];
+let lng=userLocation[1];
+
+let url="https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route="
++lat+","+lng+";"+nearestShelter.lat+","+nearestShelter.lng;
+
+window.open(url);
+
+}
+
+
+function sendSOS(){
+
+alert("SOS sent to emergency services!");
+
+}
