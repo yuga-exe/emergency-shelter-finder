a04bd453666dcee06fb893eb57db3efc24e98537
yuga-exe <185314560+yuga-exe@users.noreply.github.com>
2026-03-11 22:44:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ alert("Click My Location first");
 return;
 }
 
-let phone = "7981471506";
+let phone = "+9113736262662";
 
 let message = "Emergency! I need help. My location: https://maps.google.com/?q="
 +userLocation[0]+","+userLocation[1];
